396bd0b242803be85cbea424dd2db12b8bcdc8a7
Victor Liu <victorliu716@gmail.com>
2020-03-24 10:30:52 -0500

    Style results from search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,37 +1,40 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import SearchBar from '../components/SearchBar'
-import useResults from '../hooks/useResults'
-import ResultsList from '../components/ResultsList'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
-  const [term, setTerm] = useState('')
-  const [searchApi, results, errMsg] = useResults()
+  const [term, setTerm] = useState('');
+  const [searchApi, results, errMsg] = useResults();
 
-  const filterResultsByPrice = (price) => {
+  const filterResultsByPrice = price => {
     return results.filter(result => {
-      return result.price === price
-    })
-  }
+      return result.price === price;
+    });
+  };
 
   return (
-  <View>
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errMsg ? <Text>{errMsg}</Text> : null}
+      <Text> We have found {results.length} results </Text>
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice('$')}
+          title="Cost Effective"
+        />
+        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+        <ResultsList results={filterResultsByPrice('$$$')} title="Big Baller" />
+      </ScrollView>
+    </View>
+  );
+};
 
-    <SearchBar 
-      term={term} 
-      onTermChange={setTerm}
-      onTermSubmit={() => searchApi(term)}
-    />
-    { errMsg ? <Text>{errMsg}</Text>: null}
-    <Text> We have found {results.length} results </Text>
-    <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"/>
-    <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier"/>
-    <ResultsList results={filterResultsByPrice('$$$')} title="Big Baller"/>    
-    
-  </View>
-  )
-}
+const styles = StyleSheet.create({});
 
-const styles = StyleSheet.create({})
-
-export default SearchScreen
+export default SearchScreen;
